f05c4167ff4216a3dd12f0ce5ec1fc9d94597500
Prashant Arya <steelhitman@users.noreply.github.com>
2019-02-26 01:39:43 +0530

    Update magicpin_application_developer.py

--- magicpin_application_developer.py
+++ magicpin_application_developer.py
@@ -1,16 +1,16 @@
 password=list(raw_input().strip().split(','))
-#print password
 for p in password:
     special=["*","$","_","#","=","@"]
     non=["%","!","(",")"]
     f=[0]*5
-    if len(p)<6:
+    pas=p.strip()
+    if len(pas)<6:
         print p,"Failure Password must be at least 6 characters long."
         continue
-    if len(p)>12:
+    if len(pas)>12:
         print p,"Failure Password must be at max 12 characters long."
         continue
-    for i in p:
+    for i in pas:
         if i>=chr(65) and i<=chr(90):
             f[2]+=1
         elif i>=chr(97) and i<=chr(122):
@@ -23,15 +23,15 @@ for p in password:
             f[4]+=1
             break
     if f[0]==0:
-        print p,"Failure Password must contain at least one letter from a-z."
+        print pas,"Failure Password must contain at least one letter from a-z."
     elif f[1]==0:
-        print p,"Failure Password must contain at least one letter from 0-9."
+        print pas,"Failure Password must contain at least one letter from 0-9."
     elif f[2]==0:
-        print p,"Failure Password must contain at least one letter from A-Z."
+        print pas,"Failure Password must contain at least one letter from A-Z."
     elif f[3]==0:
-        print p,"Failure Password must contain at least one letter from *$_#=@."
+        print pas,"Failure Password must contain at least one letter from *$_#=@."
     elif f[4]!=0:
-        print p,"Failure Password cannot contain %!)(."
+        print pas,"Failure Password cannot contain %!)(."
     else:
-        print p,"Success"
+        print pas,"Success"
 
